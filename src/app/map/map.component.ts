d6e071f90795253b2c6b85c01dad9efaaed7c545
Thiago Valle <zthiagovalle@gmail.com>
2022-10-28 14:08:15 -0300

    adicionado quebra de linha na mensagem da marcacao e qual a latitude e longitude

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component } from '@angular/core';
 import * as L from 'leaflet';
 
-
 const iconRetinaUrl = 'assets/marker-icon-2x.png';
 const iconUrl = 'assets/marker-icon.png';
 const shadowUrl = 'assets/marker-shadow.png';
@@ -13,14 +12,14 @@ const iconDefault = L.icon({
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   tooltipAnchor: [16, -28],
-  shadowSize: [41, 41]
+  shadowSize: [41, 41],
 });
 L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements AfterViewInit {
   private map: any;
@@ -29,19 +28,23 @@ export class MapComponent implements AfterViewInit {
   public dataHorario: string = '';
   public destino: string = '';
 
-  constructor() { }
+  constructor() {}
 
   private initMap(): void {
     this.map = L.map('map', {
       center: [-15.909398349780076, -48.00096510495518],
-      zoom: 4
+      zoom: 4,
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
 
     tiles.addTo(this.map);
   }
@@ -51,22 +54,43 @@ export class MapComponent implements AfterViewInit {
   }
 
   public generate() {
-    const latDestino = this.destino.split(' ')[0];
-    const lonDestino = this.destino.split(' ')[1];
-    const circle = L.circleMarker([Number(latDestino), Number(lonDestino)]);
-    circle.bindPopup('Destino');
-    circle.addTo(this.map);
-
-    const arrayHorarios = this.dataHorario.split('\n');
-    const arrayLat = this.dataLat.split('\n');
-    const arrayLong = this.dataLong.split('\n');
-
-    for (let index = 0; index < arrayLong.length; index++) {
-      const lat = arrayLat[index];
-      const lon = arrayLong[index];
-      const marker = L.marker([Number(lat), Number(lon)]);
-      marker.bindPopup(`Rota(${index})\nHorário: ${arrayHorarios[index]}`);
-      marker.addTo(this.map);
+    if (this.destino) {
+      const latDestino = this.destino.split(' ')[0];
+      const lonDestino = this.destino.split(' ')[1];
+      const circle = L.circleMarker([Number(latDestino), Number(lonDestino)]);
+      circle.bindPopup('Destino');
+      circle.addTo(this.map);
     }
+    if (this.dataLat && this.dataLong && this.dataHorario) {
+      const arrayHorarios = this.dataHorario.split('\n');
+      const arrayLat = this.dataLat.split('\n');
+      const arrayLong = this.dataLong.split('\n');
+
+      for (let index = 0; index < arrayLong.length; index++) {
+        const lat = arrayLat[index];
+        const lon = arrayLong[index];
+        const marker = L.marker([Number(lat), Number(lon)]);
+        marker.bindPopup(
+          this.makeRoutePopup(index, lat, lon, arrayHorarios[index])
+        );
+        marker.addTo(this.map);
+      }
+    }
+  }
+
+  public makeRoutePopup(
+    index: number,
+    lat: string,
+    long: string,
+    horario: string
+  ): string {
+    return (
+      `` +
+      `<div>Rota: ${index}</div>` +
+      `<div>Horário: ${horario}</div>` +
+      `<div>Latitude: ${lat}</div>` +
+      `<div>Longitude: ${long}</div>`
+      + ``
+    );
   }
 }
